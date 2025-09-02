1f276ce1b45e51be9806a3d11ba247e0b86ae824
TheRealVSSVSSN <Anthonyadair@hotmail.com>
2025-09-02 10:17:33 -0700

    6

---
+++ Example_Frameworks/FiveM-FSN-Framework/fsn_licenses/fxmanifest.lua
@@ -0,0 +1,21 @@
+--[[/	:FSN:	\]]--
+fx_version 'adamant'
+game 'gta5'
+
+client_script '@fsn_main/cl_utils.lua'
+server_script '@fsn_main/sv_utils.lua'
+client_script '@fsn_main/server_settings/sh_settings.lua'
+server_script '@fsn_main/server_settings/sh_settings.lua'
+server_script '@mysql-async/lib/MySQL.lua'
+--[[/	:FSN:	\]]--
+
+client_script 'client.lua'
+client_script 'cl_desk.lua'
+server_script 'sv_desk.lua'
+
+server_script 'server.lua'
+
+exports({
+  'fsn_hasLicense',
+  'fsn_getLicensePoints'
+})
