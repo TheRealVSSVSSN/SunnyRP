317a78aefa177528acb77180312c61840df4eacd
TheRealVSSVSSN <Anthonyadair@hotmail.com>
2025-09-10 13:13:58 -0700

    refactor minimap resource

--- Example_Frameworks/NoPixelServer/minimap/client.lua
+++ Example_Frameworks/NoPixelServer/minimap/client.lua
@@ -1,18 +1,46 @@
-Citizen.CreateThread(function()
+--[[
+    -- Type: Thread
+    -- Name: MapZoomSetup
+    -- Use: Applies custom minimap zoom levels.
+    -- Created: 2025-02-14
+    -- By: VSSVSSN
+--]]
+CreateThread(function()
     SetMapZoomDataLevel(0, 0.96, 0.9, 0.08, 0.0, 0.0) -- Level 0
-    SetMapZoomDataLevel(1, 1.6, 0.9, 0.08, 0.0, 0.0) -- Level 1
-    SetMapZoomDataLevel(2, 8.6, 0.9, 0.08, 0.0, 0.0) -- Level 2
+    SetMapZoomDataLevel(1, 1.6, 0.9, 0.08, 0.0, 0.0)  -- Level 1
+    SetMapZoomDataLevel(2, 8.6, 0.9, 0.08, 0.0, 0.0)  -- Level 2
     SetMapZoomDataLevel(3, 12.3, 0.9, 0.08, 0.0, 0.0) -- Level 3
     SetMapZoomDataLevel(4, 22.3, 0.9, 0.08, 0.0, 0.0) -- Level 4
 end)
 
-Citizen.CreateThread(function()
+--[[
+    -- Type: Function
+    -- Name: applyRadarZoom
+    -- Use: Keeps radar zoom consistent based on player state.
+    -- Created: 2025-02-14
+    -- By: VSSVSSN
+--]]
+local ZOOM_PED = 1100
+local ZOOM_VEHICLE = 1100
+local function applyRadarZoom()
+    local ped = PlayerPedId()
+    if IsPedInAnyVehicle(ped, false) then
+        SetRadarZoom(ZOOM_VEHICLE)
+    else
+        SetRadarZoom(ZOOM_PED)
+    end
+end
+
+--[[
+    -- Type: Thread
+    -- Name: RadarZoomLoop
+    -- Use: Periodically updates radar zoom.
+    -- Created: 2025-02-14
+    -- By: VSSVSSN
+--]]
+CreateThread(function()
     while true do
-		Citizen.Wait(1)
-		if IsPedOnFoot(GetPlayerPed(-1)) then 
-			SetRadarZoom(1100)
-		elseif IsPedInAnyVehicle(GetPlayerPed(-1), true) then
-			SetRadarZoom(1100)
-		end
+        applyRadarZoom()
+        Wait(500)
     end
 end)
