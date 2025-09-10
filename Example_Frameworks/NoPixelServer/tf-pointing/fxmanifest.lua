e1d6a9e251e8fdd9d0d9ac54de964a8f92961743
TheRealVSSVSSN <Anthonyadair@hotmail.com>
2025-09-10 16:43:55 -0700

    refactor(tf-pointing): modernize pointing emote

---
+++ Example_Frameworks/NoPixelServer/tf-pointing/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+
+--[[
+    -- Type: Metadata
+    -- Name: tf-pointing
+    -- Use: Defines resource information and scripts
+    -- Created: 2025-06-09
+    -- By: VSSVSSN
+--]]
+
+game 'gta5'
+
+description 'Modernized pointing emote for SunnyRP'
+
+author 'VSSVSSN'
+
+client_scripts {
+    '@np-errorlog/client/cl_errorlog.lua',
+    'tf-pointing.lua'
+}
