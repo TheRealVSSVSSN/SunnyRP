0d6f89a79bc4e27a1d3be98d2f501a6c6ba31807
TheRealVSSVSSN <Anthonyadair@hotmail.com>
2025-09-10 13:23:28 -0700

    refactor np-infinity resource

--- Example_Frameworks/NoPixelServer/np-infinity/fxmanifest.lua
+++ Example_Frameworks/NoPixelServer/np-infinity/fxmanifest.lua
@@ -1,19 +1,20 @@
-fx_version "adamant"
+fx_version 'cerulean'
+game 'gta5'
 
-games {"gta5"}
+lua54 'yes'
 
-description "Infinity Helper"
-
-version "1.0.0"
+description 'Infinity Helper'
+version '1.0.0'
 
 client_scripts {
-    "client/classes/*.lua",
-    "client/cl_*.lua"
+    'client/classes/*.lua',
+    'client/cl_*.lua'
 }
 
 server_scripts {
-    "server/sv_*.lua"
+    'server/sv_*.lua'
 }
 
-server_script "tests/sv_*.lua"
-client_script "tests/cl_*.lua"
+client_script 'tests/cl_*.lua'
+server_script 'tests/sv_*.lua'
+
