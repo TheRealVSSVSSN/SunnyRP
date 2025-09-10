f8648229a210351d1e2e0ab7b7ed1f04678e9a47
TheRealVSSVSSN <Anthonyadair@hotmail.com>
2025-09-10 15:45:30 -0700

    refactor(np-notepad): modernize notepad resource

--- Example_Frameworks/NoPixelServer/np-notepad/client.lua
+++ Example_Frameworks/NoPixelServer/np-notepad/client.lua
@@ -1,129 +1,131 @@
--- Settings
+--[[
+    -- Type: Client Script
+    -- Name: np-notepad/client.lua
+    -- Use: Handles client-side notepad interactions and rendering
+    -- Created: 2025-02-14
+    -- By: VSSVSSN
+--]]
+
 local guiEnabled = false
-local hasOpened = false
 local serverNotes = {}
-local Controlkey = {["generalUse"] = {38,"E"},["generalUseSecondaryWorld"] = {23,"F"}} 
+local Controlkey = { ["generalUse"] = {38, "E"}, ["generalUseSecondaryWorld"] = {23, "F"} }
 
-function openGui()
-  SetPlayerControl(PlayerId(), 0, 0)
-  guiEnabled = true
-  SetNuiFocus(true)
-  Citizen.Trace("OPENING")
-  SendNUIMessage({openSection = "openNotepad"})
+--[[
+    -- Type: Function
+    -- Name: setGui
+    -- Use: Toggles the notepad UI in read or write mode
+    -- Created: 2025-02-14
+    -- By: VSSVSSN
+--]]
+local function setGui(state, text)
+    SetPlayerControl(PlayerId(), state and 0 or 1, 0)
+    guiEnabled = state
+    SetNuiFocus(state, state)
 
-  local inveh = IsPedSittingInAnyVehicle(PlayerPedId())
-
-  TriggerEvent("notepad")
-
+    if state then
+        local msg = { openSection = text and "openNotepadRead" or "openNotepad" }
+        if text then msg.TextRead = text end
+        SendNUIMessage(msg)
+        TriggerEvent("notepad")
+    else
+        ClearPedTasks(PlayerPedId())
+        SendNUIMessage({ openSection = "close" })
+    end
 end
 
-function openGuiRead(text)
-  SetPlayerControl(PlayerId(), 0, 0)
-  guiEnabled = true
-  SetNuiFocus(true)
-  Citizen.Trace("OPENING")
-  SendNUIMessage({openSection = "openNotepadRead", TextRead = text})
-
-  TriggerEvent("notepad")
-
+local function openGui()
+    setGui(true)
 end
 
-
-function closeGui()
-  guiEnabled = false
-  ped = PlayerPedId();
-  ClearPedTasks(ped);
-  Citizen.Trace("CLOSING")
-  SetNuiFocus(false)
-  SendNUIMessage({openSection = "close"})
-  SetPlayerControl(PlayerId(), 1, 0)
+local function openGuiRead(text)
+    setGui(true, text)
 end
 
-RegisterNUICallback('close', function(data, cb)
-  closeGui()
-  cb('ok')
+local function closeGui()
+    setGui(false)
+end
+
+RegisterNUICallback('close', function(_, cb)
+    closeGui()
+    cb('ok')
 end)
 
 RegisterNUICallback('drop', function(data, cb)
-  closeGui()
-  local coords = GetEntityCoords(PlayerPedId())
-  TriggerServerEvent("server:newNote",data.noteText,coords["x"],coords["y"],coords["z"])
+    closeGui()
+    local coords = GetEntityCoords(PlayerPedId())
+    TriggerServerEvent('server:newNote', data.noteText, coords.x, coords.y, coords.z)
+    cb('ok')
 end)
 
-
-function DrawText3Ds(x,y,z, text)
-    local onScreen,_x,_y=World3dToScreen2d(x,y,z)
-    local px,py,pz=table.unpack(GetGameplayCamCoords())
-    SetTextScale(0.35, 0.35)
-    SetTextFont(4)
-    SetTextProportional(1)
-    SetTextColour(255, 255, 255, 215)
-    SetTextEntry("STRING")
-    SetTextCentre(1)
-    AddTextComponentString(text)
-    DrawText(_x,_y)
-    local factor = (string.len(text)) / 370
-    DrawRect(_x,_y+0.0125, 0.015+ factor, 0.03, 41, 11, 41, 68)
-
+local function DrawText3Ds(x, y, z, text)
+    local onScreen, _x, _y = World3dToScreen2d(x, y, z)
+    if onScreen then
+        SetTextScale(0.35, 0.35)
+        SetTextFont(4)
+        SetTextProportional(1)
+        SetTextColour(255, 255, 255, 215)
+        SetTextCentre(true)
+        BeginTextCommandDisplayText("STRING")
+        AddTextComponentSubstringPlayerName(text)
+        EndTextCommandDisplayText(_x, _y)
+        local factor = string.len(text) / 370
+        DrawRect(_x, _y + 0.0125, 0.015 + factor, 0.03, 41, 11, 41, 68)
+    end
 end
 
-Citizen.CreateThread(function()
-  while true do
-    Citizen.Wait(1)
+CreateThread(function()
+    while true do
+        Wait(1)
 
+        if #serverNotes == 0 then
+            Wait(1000)
+        else
+            local plyLoc = GetEntityCoords(PlayerPedId())
+            local closestNoteDistance = 900.0
+            local closestNoteId = 0
 
-    if #serverNotes == 0 then
-      Citizen.Wait(1000)
-    else
-      local plyLoc = GetEntityCoords(PlayerPedId())
-      local closestNoteDistance = 900.0
-      local closestNoteId = 0
-      for i = 1, #serverNotes do
-        local distance = #(plyLoc - vector3( serverNotes[i]["x"],serverNotes[i]["y"],serverNotes[i]["z"]))
+            for i = 1, #serverNotes do
+                local note = serverNotes[i]
+                local distance = #(plyLoc - vector3(note.x, note.y, note.z))
 
-        if distance < 10.0 then
-            DrawMarker(27, serverNotes[i]["x"],serverNotes[i]["y"],serverNotes[i]["z"]-0.8, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.2, 0.2, 2.0, 255, 255,150, 75, 0, 0, 2, 0, 0, 0, 0)
-        end
+                if distance < 10.0 then
+                    DrawMarker(27, note.x, note.y, note.z - 0.8, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.2, 0.2, 2.0, 255, 255, 150, 75, 0, 0, 2, 0, 0, 0, 0)
+                end
 
-        if distance < closestNoteDistance then
-          closestNoteDistance = distance
-          closestNoteId = i
-        end
-      end
-
-      if closestNoteDistance > 100.0 then
-        Citizen.Wait(math.ceil(closestNoteDistance*10))
-      end
-      if serverNotes[closestNoteId] ~= nil then
-        local distance = #(plyLoc - vector3( serverNotes[closestNoteId]["x"],serverNotes[closestNoteId]["y"],serverNotes[closestNoteId]["z"]))
-        if distance < 2.0 then
-            DrawMarker(27, serverNotes[closestNoteId]["x"],serverNotes[closestNoteId]["y"],serverNotes[closestNoteId]["z"]-0.8, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.3, 0.3, 2.0, 255, 255, 155, 75, 0, 0, 2, 0, 0, 0, 0)
-            DrawText3Ds(serverNotes[closestNoteId]["x"],serverNotes[closestNoteId]["y"],serverNotes[closestNoteId]["z"]-0.4, "~g~"..Controlkey["generalUse"][2].."~s~ to read,~g~"..Controlkey["generalUseSecondaryWorld"][2].."~s~ to destroy")
-
-            if IsControlJustReleased(0, Controlkey["generalUse"][1]) then
-                openGuiRead(serverNotes[closestNoteId]["text"])
-            end
-            if IsControlJustReleased(0, Controlkey["generalUseSecondaryWorld"][1]) then
-              TriggerServerEvent("server:destroyNote",closestNoteId)
+                if distance < closestNoteDistance then
+                    closestNoteDistance = distance
+                    closestNoteId = i
+                end
             end
 
+            if closestNoteDistance > 100.0 then
+                Wait(math.ceil(closestNoteDistance * 10))
+            end
+
+            local note = serverNotes[closestNoteId]
+            if note then
+                local distance = #(plyLoc - vector3(note.x, note.y, note.z))
+                if distance < 2.0 then
+                    DrawMarker(27, note.x, note.y, note.z - 0.8, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.3, 0.3, 2.0, 255, 255, 155, 75, 0, 0, 2, 0, 0, 0, 0)
+                    DrawText3Ds(note.x, note.y, note.z - 0.4, "~g~" .. Controlkey["generalUse"][2] .. "~s~ to read,~g~" .. Controlkey["generalUseSecondaryWorld"][2] .. "~s~ to destroy")
+
+                    if IsControlJustReleased(0, Controlkey["generalUse"][1]) then
+                        openGuiRead(note.text)
+                    end
+                    if IsControlJustReleased(0, Controlkey["generalUseSecondaryWorld"][1]) then
+                        TriggerServerEvent('server:destroyNote', closestNoteId)
+                    end
+                end
+            elseif closestNoteId ~= 0 then
+                table.remove(serverNotes, closestNoteId)
+            end
         end
-      else
-        if serverNotes[closestNoteId] ~= nil then
-          table.remove(serverNotes,closestNoteId)
-        end
-      end
     end
-    
-  end
-
 end)
-
-
 
 RegisterNetEvent('Notepad:close')
 AddEventHandler('Notepad:close', function()
-  closeGui()
+    closeGui()
 end)
 
 RegisterNetEvent('client:updateNotes')
@@ -133,25 +135,23 @@ end)
 
 RegisterNetEvent('client:updateNotesAdd')
 AddEventHandler('client:updateNotesAdd', function(newNote)
-    serverNotes[#serverNotes+1] = newNote 
+    serverNotes[#serverNotes + 1] = newNote
 end)
+
 RegisterNetEvent('client:updateNotesRemove')
 AddEventHandler('client:updateNotesRemove', function(id)
-    table.remove(serverNotes,id)
+    table.remove(serverNotes, id)
 end)
-
-
 
 RegisterNetEvent('Notepad:open')
 AddEventHandler('Notepad:open', function()
-    local veh = GetVehiclePedIsUsing(PlayerPedId())  
+    local veh = GetVehiclePedIsUsing(PlayerPedId())
     if GetPedInVehicleSeat(veh, -1) ~= PlayerPedId() then
-      openGui()
-      guiEnabled = true
+        openGui()
     end
 end)
 
 CreateThread(function()
-  Wait(200)
-  TriggerServerEvent("server:requestNotes")
+    Wait(200)
+    TriggerServerEvent('server:requestNotes')
 end)
--- Example_Frameworks/NoPixelServer/np-notepad/server.lua
+++ Example_Frameworks/NoPixelServer/np-notepad/server.lua
@@ -1,23 +1,32 @@
+--[[
+    -- Type: Server Script
+    -- Name: np-notepad/server.lua
+    -- Use: Manages notepad storage and distribution to clients
+    -- Created: 2025-02-14
+    -- By: VSSVSSN
+--]]
+
 local serverNotes = {}
 
-RegisterServerEvent("server:destroyNote")
-AddEventHandler("server:destroyNote", function(closestNoteId)
-    table.remove(serverNotes,closestNoteId)
-    TriggerClientEvent('client:updateNotesRemove', -1, closestNoteId)
+RegisterNetEvent('server:destroyNote', function(id)
+    if serverNotes[id] then
+        table.remove(serverNotes, id)
+        TriggerClientEvent('client:updateNotesRemove', -1, id)
+    end
 end)
 
-RegisterServerEvent("server:newNote")
-AddEventHandler("server:newNote", function(text,x,y,z)
-    serverNotes[#serverNotes+1] = { text = text, x = x, y = y, z = z }
-    TriggerClientEvent('client:updateNotesAdd', -1, { text = text, x = x, y = y, z = z })
+RegisterNetEvent('server:newNote', function(text, x, y, z)
+    if type(text) ~= 'string' then return end
+    text = text:sub(1, 500)
+    local note = { text = text, x = x, y = y, z = z }
+    serverNotes[#serverNotes + 1] = note
+    TriggerClientEvent('client:updateNotesAdd', -1, note)
 end)
 
-RegisterServerEvent("server:requestNotes")
-AddEventHandler("server:requestNotes", function()
-    local src = source
-    TriggerClientEvent('client:updateNotes', src, serverNotes)
+RegisterNetEvent('server:requestNotes', function()
+    TriggerClientEvent('client:updateNotes', source, serverNotes)
 end)
 
---RegisterCommand('notepad', function(source)
---    TriggerClientEvent('Notepad:open', source)
---end)
+RegisterCommand('notepad', function(src)
+    TriggerClientEvent('Notepad:open', src)
+end)
