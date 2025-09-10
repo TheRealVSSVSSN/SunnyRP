43109125335bf674685d573b759b8aa4b96d3b36
TheRealVSSVSSN <Anthonyadair@hotmail.com>
2025-09-10 15:46:52 -0700

    refactor(np-prison): migrate to fxmanifest

---
+++ Example_Frameworks/NoPixelServer/np-prison/fxmanifest.lua
@@ -0,0 +1,24 @@
+--[[
+    -- Type: Resource Manifest
+    -- Name: np-prison
+    -- Use: Declares prison map assets for FiveM
+    -- Created: 2024-06-02
+    -- By: VSSVSSN
+--]]
+
+fx_version 'cerulean'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+files {
+    'stream/*.ytyp',
+    'stream/*.ymap',
+    'stream/*.ymf',
+    'stream/*.ydr',
+    'stream/*.ybn'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/breze_prison.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/prison_cafeteria_high.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/prison_visit_high.ytyp'
