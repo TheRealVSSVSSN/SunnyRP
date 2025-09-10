be9f9d425d4d9ec73b0f465352d14ce3f39d5565
TheRealVSSVSSN <Anthonyadair@hotmail.com>
2025-09-10 15:45:40 -0700

    refactor(np-oCam): modernize camera script

--- Example_Frameworks/NoPixelServer/np-oCam/client.lua
+++ Example_Frameworks/NoPixelServer/np-oCam/client.lua
@@ -1,78 +1,92 @@
-local INPUT_AIM = 0
-local INPUT_AIM = 0
-local UseFPS = false
-local justpressed = 0
+--[[
+    -- Type: Client Script
+    -- Name: np-oCam
+    -- Use: Handles camera view toggling and disables combat controls when needed
+    -- Created: 09/10/2025
+    -- By: VSSVSSN
+--]]
 
--- this prevents certain camera modes
+local INPUT_AIM = 25
+local useFPS = false
+local justPressed = 0
 local disable = 0
-Citizen.CreateThread( function()
 
-  while true do    
-    
-    Citizen.Wait(1)
-
+--[[
+    -- Type: Function
+    -- Name: handleCameraView
+    -- Use: Toggles first-person view on quick aim and blocks unwanted camera modes
+    -- Created: 09/10/2025
+    -- By: VSSVSSN
+--]]
+local function handleCameraView()
+    while true do
+        Wait(0)
 
         if IsControlPressed(0, INPUT_AIM) then
-          justpressed = justpressed + 1
+            justPressed = justPressed + 1
         end
 
         if IsControlJustReleased(0, INPUT_AIM) then
-
-        	if justpressed < 15 then
-        		UseFPS = true
-        	end
-        	justpressed = 0
+            if justPressed < 15 then
+                useFPS = true
+            end
+            justPressed = 0
         end
 
         if GetFollowPedCamViewMode() == 1 or GetFollowVehicleCamViewMode() == 1 then
-        	Citizen.Wait(1)
-        	SetFollowPedCamViewMode(0)
-        	SetFollowVehicleCamViewMode(0)
+            SetFollowPedCamViewMode(0)
+            SetFollowVehicleCamViewMode(0)
         end
 
-
-        if UseFPS then
-        	if GetFollowPedCamViewMode() == 0 or GetFollowVehicleCamViewMode() == 0 then
-        		Citizen.Wait(1)
-        		
-        		SetFollowPedCamViewMode(4)
-        		SetFollowVehicleCamViewMode(4)
-        	else
-        		Citizen.Wait(1)
-        		
-        		SetFollowPedCamViewMode(0)
-        		SetFollowVehicleCamViewMode(0)
-        	end
-    		UseFPS = false
+        if useFPS then
+            if GetFollowPedCamViewMode() == 0 or GetFollowVehicleCamViewMode() == 0 then
+                SetFollowPedCamViewMode(4)
+                SetFollowVehicleCamViewMode(4)
+            else
+                SetFollowPedCamViewMode(0)
+                SetFollowVehicleCamViewMode(0)
+            end
+            useFPS = false
         end
 
-
-        if IsPedArmed(ped,1) or not IsPedArmed(ped,7) then
-            if IsControlJustPressed(0,24) or IsControlJustPressed(0,141) or IsControlJustPressed(0,142) or IsControlJustPressed(0,140)  then
-               disable = 50
+        local ped = PlayerPedId()
+        if IsPedArmed(ped, 1) or not IsPedArmed(ped, 7) then
+            if IsControlJustPressed(0, 24) or IsControlJustPressed(0, 141) or IsControlJustPressed(0, 142) or IsControlJustPressed(0, 140) then
+                disable = 50
             end
         end
 
         if disable > 0 then
             disable = disable - 1
-            DisableControlAction(0,24)
-            DisableControlAction(0,140)
-            DisableControlAction(0,141)
-            DisableControlAction(0,142)
+            DisableControlAction(0, 24, true)
+            DisableControlAction(0, 140, true)
+            DisableControlAction(0, 141, true)
+            DisableControlAction(0, 142, true)
         end
-  end
+    end
+end
 
-end)
-
-Citizen.CreateThread(function()
+--[[
+    -- Type: Function
+    -- Name: disableMeleeWhileArmed
+    -- Use: Disables melee controls when player is armed
+    -- Created: 09/10/2025
+    -- By: VSSVSSN
+--]]
+local function disableMeleeWhileArmed()
     while true do
-        Citizen.Wait(1)
-        if IsPedArmed(PlayerPedId(), 6) then
+        local ped = PlayerPedId()
+        if IsPedArmed(ped, 6) then
             DisableControlAction(1, 140, true)
             DisableControlAction(1, 141, true)
             DisableControlAction(1, 142, true)
+            Wait(0)
         else
-            Citizen.Wait(1500)
+            Wait(1500)
         end
     end
-end)
+end
+
+CreateThread(handleCameraView)
+CreateThread(disableMeleeWhileArmed)
+
---
+++ Example_Frameworks/NoPixelServer/np-oCam/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+client_scripts {
+    '@np-errorlog/client/cl_errorlog.lua',
+    'client.lua'
+}
+
