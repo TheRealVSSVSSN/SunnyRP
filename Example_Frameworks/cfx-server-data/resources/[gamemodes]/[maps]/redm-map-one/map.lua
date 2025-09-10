cd24708c2749336637854dcb2e4a722dc60d3004
TheRealVSSVSSN <Anthonyadair@hotmail.com>
2025-09-10 09:23:18 -0700

    refactor: modernize redm map resource

--- Example_Frameworks/cfx-server-data/resources/[gamemodes]/[maps]/redm-map-one/map.lua
+++ Example_Frameworks/cfx-server-data/resources/[gamemodes]/[maps]/redm-map-one/map.lua
@@ -1,2 +1,22 @@
-spawnpoint 'player_three' { x = -262.849, y = 793.404, z = 118.087 }
-spawnpoint 'player_zero' { x = -262.849, y = 793.404, z = 118.087 }
+--[[
+    -- Type: Script
+    -- Name: map.lua
+    -- Use: Registers spawn points for RedM
+    -- Updated: 2025-02-14
+    -- By: VSSVSSN
+--]]
+
+local spawnPoints = {
+    { model = 'player_three', coords = vector4(-262.849, 793.404, 118.087, 180.0) },
+    { model = 'player_zero',  coords = vector4(-262.849, 793.404, 118.087, 180.0) }
+}
+
+for _, sp in ipairs(spawnPoints) do
+    spawnpoint(sp.model, {
+        x = sp.coords.x,
+        y = sp.coords.y,
+        z = sp.coords.z,
+        heading = sp.coords.w
+    })
+end
+
