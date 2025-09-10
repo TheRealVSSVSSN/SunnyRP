dec36156db58992acd9120d810e97fec1607e610
TheRealVSSVSSN <Anthonyadair@hotmail.com>
2025-09-10 15:48:23 -0700

    refactor: modernize np-sirens

--- Example_Frameworks/NoPixelServer/np-sirens/fxmanifest.lua
+++ Example_Frameworks/NoPixelServer/np-sirens/fxmanifest.lua
@@ -1,7 +1,12 @@
 fx_version 'cerulean'
+game 'gta5'
 
-games { 'gta5' }
+lua54 'yes'
+
+author 'SunnyRP'
+description 'Disable distant police sirens to reduce ambient noise and improve performance'
+version '1.0.0'
 
 client_scripts {
-	'client/cl_*.lua',
+    'client/cl_sirens.lua'
 }
