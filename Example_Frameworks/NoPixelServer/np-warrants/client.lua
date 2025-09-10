28a8b08ffc328366ac3cd0f18582b61cc85b3153
TheRealVSSVSSN <Anthonyadair@hotmail.com>
2025-09-10 15:52:05 -0700

    refactor np-warrants and update manifest

--- Example_Frameworks/NoPixelServer/np-warrants/client.lua
+++ Example_Frameworks/NoPixelServer/np-warrants/client.lua
@@ -1,85 +1,82 @@
-local guiEnabled = false
+local guiEnabled, hasNuiFocus = false, false
 
--- Open Gui and Focus NUI
-function openGui()
-  local isInVeh = IsPedInAnyVehicle(PlayerPedId(), false)
-  --if not isInVeh then
-  --  SetPlayerControl(PlayerId(), 0, 0)
-  --end
-  guiEnabled = true
-  SetCustomNuiFocus(true,true)
-  SendNUIMessage({openWarrants = true})
-   TriggerEvent('animation:tablet',true)
+--[[
+    -- Type: Function
+    -- Name: setCustomNuiFocus
+    -- Use: Handles NUI focus and voice settings
+    -- Created: 2024-11-14
+    -- By: VSSVSSN
+--]]
+local function setCustomNuiFocus(hasKeyboard, hasMouse)
+    hasNuiFocus = hasKeyboard or hasMouse
+    SetNuiFocus(hasKeyboard, hasMouse)
+    SetNuiFocusKeepInput(hasNuiFocus)
+    TriggerEvent("np:voice:focus:set", hasNuiFocus, hasKeyboard, hasMouse)
 end
 
-function openGuiDoctor()
-  local isInVeh = IsPedInAnyVehicle(PlayerPedId(), false)
-  if not isInVeh then
-    SetPlayerControl(PlayerId(), 0, 0)
-  end
-  guiEnabled = true
-  SetCustomNuiFocus(true,true)
-  SendNUIMessage({openDoctors = true})
-   TriggerEvent('animation:tablet',true)
+--[[
+    -- Type: Function
+    -- Name: openGui
+    -- Use: Opens the specified MDT section
+    -- Created: 2024-11-14
+    -- By: VSSVSSN
+--]]
+local function openGui(section)
+    guiEnabled = true
+    setCustomNuiFocus(true, true)
+    local message = {}
+
+    if section == 'warrants' then
+        message.openWarrants = true
+    elseif section == 'doctors' then
+        message.openDoctors = true
+    elseif section == 'publicrecords' then
+        message.openSection = 'publicrecords'
+    end
+
+    SendNUIMessage(message)
+    TriggerEvent('animation:tablet', true)
 end
 
-function openpr()
-  SetPlayerControl(PlayerId(), 0, 0)
-  guiEnabled = true
-  SetCustomNuiFocus(true,true)
-    SendNUIMessage({
-      openSection = "publicrecords"
-    })
-   TriggerEvent('animation:tablet',true)
+--[[
+    -- Type: Function
+    -- Name: closeGui
+    -- Use: Closes MDT interface and resets focus
+    -- Created: 2024-11-14
+    -- By: VSSVSSN
+--]]
+local function closeGui()
+    setCustomNuiFocus(false, false)
+    guiEnabled = false
+    TriggerEvent('animation:tablet', false)
+    Wait(250)
+    ClearPedTasks(PlayerPedId())
 end
 
--- Close Gui and disable NUI
-function closeGui()
-  SetCustomNuiFocus(false,false)
-  guiEnabled = false
-  TriggerEvent('animation:tablet',false)
-  Wait(250)
-  ClearPedTasks(PlayerPedId())
-  SetPlayerControl(PlayerId(), 1, 0)
-end
-
+--[[ Event Registrations ]]--
 RegisterNetEvent("phone:publicrecords")
 AddEventHandler("phone:publicrecords", function()
-    openpr()
+    openGui('publicrecords')
 end)
 
--- Opens our warrants
 RegisterNetEvent('warrantsGui')
 AddEventHandler('warrantsGui', function()
-  openGui()
-  guiEnabled = true
-end)
-
-RegisterNetEvent('mdt:close')
-AddEventHandler('mdt:close', function()
-  closeGui()
-  SendNUIMessage({
-    closeGUI = true
-  })
+    openGui('warrants')
 end)
 
 RegisterNetEvent('doctorGui')
 AddEventHandler('doctorGui', function()
-  openGuiDoctor()
-  guiEnabled = true
+    openGui('doctors')
 end)
 
--- NUI Callback Methods
-RegisterNUICallback('close', function(data, cb)
-  closeGui()
-  cb('ok')
+RegisterNetEvent('mdt:close')
+AddEventHandler('mdt:close', function()
+    closeGui()
+    SendNUIMessage({ closeGUI = true })
 end)
 
-function SetCustomNuiFocus(hasKeyboard, hasMouse)
-  HasNuiFocus = hasKeyboard or hasMouse
-
-  SetNuiFocus(hasKeyboard, hasMouse)
-  SetNuiFocusKeepInput(HasNuiFocus)
-
-  TriggerEvent("np:voice:focus:set", HasNuiFocus, hasKeyboard, hasMouse)
-end
+--[[ NUI Callback ]]--
+RegisterNUICallback('close', function(_, cb)
+    closeGui()
+    cb('ok')
+end)
