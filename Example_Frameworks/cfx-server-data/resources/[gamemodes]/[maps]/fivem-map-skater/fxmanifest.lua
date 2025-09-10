c29027aa03369e0b9383c03bd26e298aa7e68068
TheRealVSSVSSN <Anthonyadair@hotmail.com>
2025-09-10 09:22:41 -0700

    refactor: modernize skater map resource

--- Example_Frameworks/cfx-server-data/resources/[gamemodes]/[maps]/fivem-map-skater/fxmanifest.lua
+++ Example_Frameworks/cfx-server-data/resources/[gamemodes]/[maps]/fivem-map-skater/fxmanifest.lua
@@ -1,14 +1,21 @@
--- This resource is part of the default Cfx.re asset pack (cfx-server-data)
--- Altering or recreating for local use only is strongly discouraged.
+--[[
+    -- Type: Manifest
+    -- Name: fivem-map-skater
+    -- Use: Defines metadata for the skater spawn map
+    -- Updated: 2024-05-31
+    -- By: VSSVSSN
+--]]
 
-version '1.0.0'
+fx_version 'cerulean'
+lua54 'yes'
+game 'gta5'
+
+version '1.1.0'
 author 'Cfx.re <root@cfx.re>'
 description 'Example spawn points for FiveM with a "skater" model.'
 repository 'https://github.com/citizenfx/cfx-server-data'
 
+this_is_a_map 'yes'
 resource_type 'map' { gameTypes = { ['basic-gamemode'] = true } }
 
 map 'map.lua'
-
-fx_version 'adamant'
-game 'gta5'
