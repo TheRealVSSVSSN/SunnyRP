ed10222e954f2e5af338f3840bd0b43d090d85e7
TheRealVSSVSSN <Anthonyadair@hotmail.com>
2025-09-10 15:47:06 -0700

    chore(np-rehab): remove legacy resource manifest

---
+++ Example_Frameworks/NoPixelServer/np-rehab/fxmanifest.lua
@@ -0,0 +1,17 @@
+--[[
+    -- Type: Manifest
+    -- Name: fxmanifest.lua
+    -- Use: Defines resource metadata for np-rehab
+    -- Created: 2024-05-31
+    -- By: VSSVSSN
+--]]
+
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+client_script '@np-errorlog/client/cl_errorlog.lua'
+
+client_script 'client.lua'
+server_script 'server.lua'
+
--- Example_Frameworks/NoPixelServer/np-rehab/server.lua
+++ Example_Frameworks/NoPixelServer/np-rehab/server.lua
@@ -1,16 +1,33 @@
+--[[ 
+    -- Type: Server
+    -- Name: np-rehab server
+    -- Use: Provides commands to manage player rehabilitation
+    -- Created: 2024-05-31
+    -- By: VSSVSSN
+--]]
+
+--[[ 
+    -- Type: Command
+    -- Name: rehab
+    -- Use: Places or removes a player from rehab
+    -- Created: 2024-05-31
+    -- By: VSSVSSN
+--]]
 RegisterCommand('rehab', function(source, args)
     local src = source
-	local user = exports["np-base"]:getModule("Player"):GetUser(src)
+    local user = exports["np-base"]:getModule("Player"):GetUser(src)
+    if not user then return end
 
-    if user:getVar("job") == 'therapist' or user:getVar("job") == 'doctor' then
-        if args[1] then
-            if args[2] == tryue then
-                TriggerClientEvent("beginJailRehab", args[1], true)
-            else
-                TriggerClientEvent("beginJailRehab", args[1], false)
-            end
-        else
-            TriggerClientEvent("DoLongHudText", src, 'There are no player with this ID.', 2)
-        end
+    local job = user:getVar("job")
+    if job ~= 'therapist' and job ~= 'doctor' then return end
+
+    local target = tonumber(args[1])
+    if not target or not GetPlayerName(target) then
+        TriggerClientEvent("DoLongHudText", src, 'There are no player with this ID.', 2)
+        return
     end
-end)
+
+    local inRehab = args[2] and args[2]:lower() == 'true'
+    TriggerClientEvent("beginJailRehab", target, inRehab)
+end, false)
+
