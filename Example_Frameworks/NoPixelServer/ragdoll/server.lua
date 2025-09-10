ee7e73ea4adb0f2ad6e0310b5b31145fc5a611d3
TheRealVSSVSSN <Anthonyadair@hotmail.com>
2025-09-10 15:54:35 -0700

    refactor ragdoll resource

--- Example_Frameworks/NoPixelServer/ragdoll/server.lua
+++ Example_Frameworks/NoPixelServer/ragdoll/server.lua
@@ -1,54 +1,65 @@
-RegisterServerEvent('police:isDead')
-AddEventHandler('police:isDead', function()
-    -- local src = source
-	-- local user = exports["np-base"]:getModule("Player"):GetUser(src)
-    -- local characterId = user:getVar("character").id
-    -- local q = [[SELECT is_dead FROM characters WHERE id = @cid;]]
-    -- local v = {
-    --     ["isDead"] = newLevel,
-    --     ["cid"] = characterId
-    -- }
-    -- local sex = false
-    -- exports.ghmattimysql:execute(q, v, function(result)
-    --     if not result[1].is_dead == nil then
-    --         TriggerClientEvent('syd:isdead', source, result[1].is_dead)
-    --         TriggerClientEvent('updatedead', source, result[1].is_dead)
-    --     end
-    -- end)
+--[[
+    -- Type: Server Script
+    -- Name: Ragdoll Server Handlers
+    -- Use: Manages death state and revival logic
+    -- Created: 2024-02-23
+    -- By: VSSVSSN
+--]]
+
+local CPR_PRICE = 10000
+
+--[[
+    -- Type: Event
+    -- Name: ragdoll:playerDied
+    -- Use: Placeholder for logging when a player dies
+--]]
+RegisterNetEvent('ragdoll:playerDied', function(cause)
+    -- future implementation: log cause or update database
 end)
 
-RegisterServerEvent('kill:confirmed')
-AddEventHandler('kill:confirmed', function(targetplayer, data)
-    -- local src = source
-	-- local user = exports["np-base"]:getModule("Player"):GetUser(src)
-    -- local characterId = user:getVar("character").id
-    -- local q = [[UPDATE characters SET is_dead = @isDead WHERE id = @cid;]]
-    -- local v = {
-    --     ["isDead"] = data,
-    --     ["cid"] = characterId
-    -- }
-    -- exports.ghmattimysql:execute(q, v, function()
-    -- end)
-end)
-
-RegisterServerEvent('reviveGranted')
-AddEventHandler('reviveGranted', function(t)
-	TriggerClientEvent('reviveFunction', t)
-end)
-
-RegisterServerEvent('trycpr')
-AddEventHandler('trycpr', function()
-	local user = exports["np-base"]:getModule("Player"):GetUser(src)
-    local price = 10000
-    if user:getCash() >= price then
-        user:removeMoney(price)
-        TriggerClientEvent('trycpr', source)
-    else
-        TriggerClientEvent('DoLongHudText', source, "You can't afford that CPR", 2)
+--[[
+    -- Type: Event
+    -- Name: ragdoll:confirmKill
+    -- Use: Notifies a target client that they have been killed
+--]]
+RegisterNetEvent('ragdoll:confirmKill', function(target)
+    if target then
+        TriggerClientEvent('ragdoll:setDead', target)
     end
 end)
 
-RegisterServerEvent('serverCPR')
-AddEventHandler('serverCPR', function()
-	TriggerClientEvent('revive', source)
+--[[
+    -- Type: Event
+    -- Name: ragdoll:grantRevive
+    -- Use: Revives a target client
+--]]
+RegisterNetEvent('ragdoll:grantRevive', function(target)
+    TriggerClientEvent('ragdoll:revive', target)
+end)
+
+--[[
+    -- Type: Event
+    -- Name: ragdoll:tryCPR
+    -- Use: Charges player and triggers CPR animation
+--]]
+RegisterNetEvent('ragdoll:tryCPR', function()
+    local src = source
+    local user = exports['np-base']:getModule('Player'):GetUser(src)
+    if not user then return end
+
+    if user:getCash() >= CPR_PRICE then
+        user:removeMoney(CPR_PRICE)
+        TriggerClientEvent('ragdoll:doCPR', src)
+    else
+        TriggerClientEvent('DoLongHudText', src, "You can't afford that CPR", 2)
+    end
+end)
+
+--[[
+    -- Type: Event
+    -- Name: ragdoll:serverCPR
+    -- Use: Used after successful CPR to revive the player
+--]]
+RegisterNetEvent('ragdoll:serverCPR', function()
+    TriggerClientEvent('ragdoll:revive', source)
 end)
