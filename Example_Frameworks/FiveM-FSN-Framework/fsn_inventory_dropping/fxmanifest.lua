110cfed9136ccbabf4faa632bf5ff6517719c5e7
TheRealVSSVSSN <Anthonyadair@hotmail.com>
2025-09-10 16:58:26 -0700

    refactor: modernize inventory dropping

--- Example_Frameworks/FiveM-FSN-Framework/fsn_inventory_dropping/fxmanifest.lua
+++ Example_Frameworks/FiveM-FSN-Framework/fsn_inventory_dropping/fxmanifest.lua
@@ -1,13 +1,18 @@
---[[/	:FSN:	\]]--
-fx_version 'adamant'
+--[[
+    -- Type: Manifest
+    -- Name: fxmanifest
+    -- Use: Registers inventory drop scripts
+    -- Created: 2024-09-17
+    -- By: VSSVSSN
+--]]
+
+fx_version 'cerulean'
 game 'gta5'
+lua54 'yes'
 
 client_script '@fsn_main/cl_utils.lua'
 server_script '@fsn_main/sv_utils.lua'
-client_script '@fsn_main/server_settings/sh_settings.lua'
-server_script '@fsn_main/server_settings/sh_settings.lua'
-server_script '@mysql-async/lib/MySQL.lua'
---[[/	:FSN:	\]]--
+shared_script '@fsn_main/server_settings/sh_settings.lua'
 
 client_script 'cl_dropping.lua'
 server_script 'sv_dropping.lua'
