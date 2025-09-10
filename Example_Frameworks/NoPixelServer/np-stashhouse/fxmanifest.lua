a3967e047e0612520cd0db28cd02b1dbb5fc4a88
TheRealVSSVSSN <Anthonyadair@hotmail.com>
2025-09-10 15:49:16 -0700

    refactor: modernize stashhouse resource manifest

---
+++ Example_Frameworks/NoPixelServer/np-stashhouse/fxmanifest.lua
@@ -0,0 +1,22 @@
+--[[ 
+    -- Type: Resource Manifest
+    -- Name: fxmanifest.lua
+    -- Use: Defines map resource configuration for np-stashhouse
+    -- Created: 2025-09-10
+    -- By: VSSVSSN
+--]]
+
+fx_version 'cerulean'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+files {
+    'stream/_manifest.ymf',
+    'stream/*.ytyp',
+    'stream/*.ymap',
+    'stream/*.ydr',
+    'stream/*.ytd'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/shellpropsv15.ytyp'
