f3ba86aeaaaaa7f308157fcfefd6628284269ab8
TheRealVSSVSSN <Anthonyadair@hotmail.com>
2025-09-10 15:53:51 -0700

    refactor: modernize radio resource

--- Example_Frameworks/NoPixelServer/radio/client/main.lua
+++ Example_Frameworks/NoPixelServer/radio/client/main.lua
@@ -1,88 +1,55 @@
 
-local GuiOpened = false
-
-RegisterNetEvent('radioGui')
-AddEventHandler('radioGui', function()
-  openGui()
-end)
-RegisterNetEvent('ChannelSet')
-AddEventHandler('ChannelSet', function(chan)
-  SendNUIMessage({set = true, setChannel = chan})
-end)
-
-RegisterNetEvent('radio:resetNuiCommand')
-AddEventHandler('radio:resetNuiCommand', function()
-    SendNUIMessage({reset = true})
-end)
+local GuiOpened, inPhone, HasNuiFocus = false, false, false
 
 local function formattedChannelNumber(number)
   local power = 10 ^ 1
   return math.floor(number * power) / power
 end
 
-function closeGui()
-  TriggerEvent("InteractSound_CL:PlayOnOne","radioclick",0.6)
+local function closeGui()
+  TriggerEvent("InteractSound_CL:PlayOnOne", "radioclick", 0.6)
   GuiOpened = false
-  SetCustomNuiFocus(false,false)
-  SendNUIMessage({open = false})
-  TriggerEvent("animation:radio",GuiOpened)
+  SetCustomNuiFocus(false, false)
+  SendNUIMessage({ open = false })
+  TriggerEvent("animation:radio", false)
 end
 
-function openGui()
-  local radio = hasRadio()
-  local incall = exports["isPed"]:isPed("incall")
-  if (incall) then
-   TriggerEvent("DoShortHudText","You can not do that while in a call!",2)
-   return
- end
- local job = exports["isPed"]:isPed("myjob")
- local Emergency = false
- if job == "police" then
-   Emergency = true
- elseif job == "ems" then
-   Emergency = true
- elseif job == "doctor" then
-   Emergency = true
- end
+local function hasRadio()
+  return exports["np-inventory"]:hasEnoughOfItem("radio", 1, false)
+end
 
-
-  
-  if not GuiOpened and radio then
-    GuiOpened = true
-    SetCustomNuiFocus(false,false)
-    SetCustomNuiFocus(true,true)
-    SendNUIMessage({open = true, jobType = Emergency})
-  else
-    GuiOpened = false
-    SetCustomNuiFocus(false,false)
-    SendNUIMessage({open = false, jobType = Emergency})
+local function openGui()
+  if exports["isPed"]:isPed("incall") then
+    TriggerEvent("DoShortHudText", "You can not do that while in a call!", 2)
+    return
   end
-  TriggerEvent("animation:radio",GuiOpened)
-end
 
-function hasRadio()
-  if exports["np-inventory"]:hasEnoughOfItem("radio",1,false) then
-    return true
-  else
-    return false
-  end
+  if not hasRadio() then return end
+
+  local job = exports["isPed"]:isPed("myjob")
+  local Emergency = job == "police" or job == "ems" or job == "doctor"
+
+  GuiOpened = not GuiOpened
+  SetCustomNuiFocus(GuiOpened, GuiOpened)
+  SendNUIMessage({ open = GuiOpened, jobType = Emergency })
+  TriggerEvent("animation:radio", GuiOpened)
 end
-RegisterNUICallback('click', function(data, cb)
+RegisterNUICallback('click', function(_, cb)
   PlaySound(-1, "NAV_UP_DOWN", "HUD_FRONTEND_DEFAULT_SOUNDSET", 0, 0, 1)
   cb('ok')
 end)
 
-RegisterNUICallback('volumeUp', function(data, cb)
+RegisterNUICallback('volumeUp', function(_, cb)
   exports["np-voice"]:IncreaseRadioVolume()
   cb('ok')
 end)
 
-RegisterNUICallback('volumeDown', function(data, cb)
+RegisterNUICallback('volumeDown', function(_, cb)
   exports["np-voice"]:DecreaseRadioVolume()
   cb('ok')
 end)
 
-RegisterNUICallback('cleanClose', function(data, cb)
+RegisterNUICallback('cleanClose', function(_, cb)
   closeGui()
   cb('ok')
 end)
@@ -97,14 +64,14 @@ local function handleConnectionEvent(pChannel)
   end
 end
 
-RegisterNUICallback('poweredOn', function(data, cb)
-  TriggerEvent("InteractSound_CL:PlayOnOne","radioon",0.6)
+RegisterNUICallback('poweredOn', function(_, cb)
+  TriggerEvent("InteractSound_CL:PlayOnOne", "radioon", 0.6)
   exports["np-voice"]:SetRadioPowerState(true)
   cb('ok')
 end)
 
-RegisterNUICallback('poweredOff', function(data, cb)
-  TriggerEvent("InteractSound_CL:PlayOnOne","radiooff",0.6)
+RegisterNUICallback('poweredOff', function(_, cb)
+  TriggerEvent("InteractSound_CL:PlayOnOne", "radiooff", 0.6)
   exports["np-voice"]:SetRadioPowerState(false)
   cb('ok')
 end)
@@ -124,43 +91,41 @@ AddEventHandler('animation:radio', function(enable)
 
   RequestAnimDict("cellphone@")
   while not HasAnimDictLoaded("cellphone@") do
-    Citizen.Wait(0)
+    Wait(0)
   end
 
   local intrunk = exports["isPed"]:isPed("intrunk")
   if not intrunk then
     TaskPlayAnim(lPed, "cellphone@", "cellphone_text_in", 2.0, 3.0, -1, 49, 0, 0, 0, 0)
   end
-  Citizen.Wait(300)
+  Wait(300)
   if inPhone then
-    TriggerEvent("attachItemRadio","radio01")
-    Citizen.Wait(150)
+    TriggerEvent("attachItemRadio", "radio01")
+    Wait(150)
     while inPhone do
-
-      local dead = exports["isPed"]:isPed("dead")
-      if dead then
+      if exports["isPed"]:isPed("dead") then
         closeGui()
         inPhone = false
       end
-      local intrunk = exports["isPed"]:isPed("intrunk")
+      intrunk = exports["isPed"]:isPed("intrunk")
       if not intrunk and not IsEntityPlayingAnim(lPed, "cellphone@", "cellphone_text_read_base", 3) and not IsEntityPlayingAnim(lPed, "cellphone@", "cellphone_swipe_screen", 3) then
         TaskPlayAnim(lPed, "cellphone@", "cellphone_text_read_base", 2.0, 3.0, -1, 49, 0, 0, 0, 0)
-      end    
-      Citizen.Wait(500)
+      end
+      Wait(500)
     end
-    local intrunk = exports["isPed"]:isPed("intrunk")
+    intrunk = exports["isPed"]:isPed("intrunk")
     if not intrunk then
       ClearPedTasks(PlayerPedId())
     end
   else
-    local intrunk = exports["isPed"]:isPed("intrunk")
+    intrunk = exports["isPed"]:isPed("intrunk")
     if not intrunk then
-      Citizen.Wait(100)
+      Wait(100)
       ClearPedTasks(PlayerPedId())
       TaskPlayAnim(lPed, "cellphone@", "cellphone_text_out", 2.0, 1.0, 5.0, 49, 0, 0, 0, 0)
-      Citizen.Wait(400)
+      Wait(400)
       TriggerEvent("destroyPropRadio")
-      Citizen.Wait(400)
+      Wait(400)
       ClearPedTasks(PlayerPedId())
     else
       TriggerEvent("destroyPropRadio")
@@ -169,36 +134,46 @@ AddEventHandler('animation:radio', function(enable)
   TriggerEvent("destroyPropRadio")
 end)
 
-Citizen.CreateThread(function()
-
+CreateThread(function()
   while true do
     if GuiOpened then
-      Citizen.Wait(1)
-      DisableControlAction(0, 1, GuiOpened) -- LookLeftRight
-      DisableControlAction(0, 2, GuiOpened) -- LookUpDown
-      DisableControlAction(0, 14, GuiOpened) -- INPUT_WEAPON_WHEEL_NEXT
-      DisableControlAction(0, 15, GuiOpened) -- INPUT_WEAPON_WHEEL_PREV
-      DisableControlAction(0, 16, GuiOpened) -- INPUT_SELECT_NEXT_WEAPON
-      DisableControlAction(0, 17, GuiOpened) -- INPUT_SELECT_PREV_WEAPON
-      DisableControlAction(0, 99, GuiOpened) -- INPUT_VEH_SELECT_NEXT_WEAPON
-      DisableControlAction(0, 100, GuiOpened) -- INPUT_VEH_SELECT_PREV_WEAPON
-      DisableControlAction(0, 115, GuiOpened) -- INPUT_VEH_FLY_SELECT_NEXT_WEAPON
-      DisableControlAction(0, 116, GuiOpened) -- INPUT_VEH_FLY_SELECT_PREV_WEAPON
-      DisableControlAction(0, 142, GuiOpened) -- MeleeAttackAlternate
-      DisableControlAction(0, 106, GuiOpened) -- VehicleMouseControlOverride
+      Wait(0)
+      DisableControlAction(0, 1, true) -- LookLeftRight
+      DisableControlAction(0, 2, true) -- LookUpDown
+      DisableControlAction(0, 14, true) -- INPUT_WEAPON_WHEEL_NEXT
+      DisableControlAction(0, 15, true) -- INPUT_WEAPON_WHEEL_PREV
+      DisableControlAction(0, 16, true) -- INPUT_SELECT_NEXT_WEAPON
+      DisableControlAction(0, 17, true) -- INPUT_SELECT_PREV_WEAPON
+      DisableControlAction(0, 99, true) -- INPUT_VEH_SELECT_NEXT_WEAPON
+      DisableControlAction(0, 100, true) -- INPUT_VEH_SELECT_PREV_WEAPON
+      DisableControlAction(0, 115, true) -- INPUT_VEH_FLY_SELECT_NEXT_WEAPON
+      DisableControlAction(0, 116, true) -- INPUT_VEH_FLY_SELECT_PREV_WEAPON
+      DisableControlAction(0, 142, true) -- MeleeAttackAlternate
+      DisableControlAction(0, 106, true) -- VehicleMouseControlOverride
     else
-      Citizen.Wait(20)
-    end    
+      Wait(200)
+    end
   end
 end)
 
 
-function SetCustomNuiFocus(hasKeyboard, hasMouse)
-  HasNuiFocus = hasKeyboard or hasMouse
+  function SetCustomNuiFocus(hasKeyboard, hasMouse)
+    HasNuiFocus = hasKeyboard or hasMouse
+    SetNuiFocus(hasKeyboard, hasMouse)
+    SetNuiFocusKeepInput(HasNuiFocus)
+    TriggerEvent("np-voice:focus:set", HasNuiFocus, hasKeyboard, hasMouse)
+  end
 
-  SetNuiFocus(hasKeyboard, hasMouse)
-  SetNuiFocusKeepInput(HasNuiFocus)
+RegisterNetEvent('radioGui')
+AddEventHandler('radioGui', openGui)
 
-  TriggerEvent("np-voice:focus:set", HasNuiFocus, hasKeyboard, hasMouse)
-end
+RegisterNetEvent('ChannelSet')
+AddEventHandler('ChannelSet', function(chan)
+  SendNUIMessage({ set = true, setChannel = chan })
+end)
+
+RegisterNetEvent('radio:resetNuiCommand')
+AddEventHandler('radio:resetNuiCommand', function()
+  SendNUIMessage({ reset = true })
+end)
 
---
+++ Example_Frameworks/NoPixelServer/radio/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+client_script '@np-errorlog/client/cl_errorlog.lua'
+
+ui_page 'html/ui.html'
+
+files {
+    'html/ui.html',
+    'html/background.png',
+    'html/scripts.js',
+    'html/styles.css',
+    'html/pricedown.ttf'
+}
+
+client_scripts {
+    'client/main.lua'
+}
