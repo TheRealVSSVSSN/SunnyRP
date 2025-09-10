7ee11574d3e84d30f07610ac9cf9fb1427fdc23d
TheRealVSSVSSN <Anthonyadair@hotmail.com>
2025-09-10 13:14:22 -0700

    refactor(noclip): modernize and fix speed handling

---
+++ Example_Frameworks/NoPixelServer/noclip/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'SunnyRP'
+description 'Improved noclip utility'
+version '1.0.0'
+
+client_scripts {
+    'Instructional_Buttons.lua',
+    'noclip.lua'
+}
+
