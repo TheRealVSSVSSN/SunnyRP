596608108a5e8e07a3c014df6999cf9b79ffffa8
TheRealVSSVSSN <Anthonyadair@hotmail.com>
2025-09-10 15:50:58 -0700

    refactor: update tunershop resource

--- Example_Frameworks/NoPixelServer/np-tunershop/client.lua
+++ Example_Frameworks/NoPixelServer/np-tunershop/client.lua
@@ -1,5 +1,14 @@
+--[[
+    -- Type: Client Script
+    -- Name: client.lua
+    -- Use: Initializes the tuner shop warehouse IPLs on client start
+    -- Created: 2025-09-10
+    -- By: VSSVSSN
+--]]
 
-Citizen.CreateThread(function()
-    ImportVehicleWarehouse.LoadDefault()
+CreateThread(function()
+    if ImportVehicleWarehouse and ImportVehicleWarehouse.LoadDefault then
+        ImportVehicleWarehouse.LoadDefault()
+    end
 end)
 
