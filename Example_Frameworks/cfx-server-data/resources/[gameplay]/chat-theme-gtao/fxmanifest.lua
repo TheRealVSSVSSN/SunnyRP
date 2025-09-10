048976b780d054641875f1e8c13501110ad70792
TheRealVSSVSSN <Anthonyadair@hotmail.com>
2025-09-10 09:36:35 -0700

    refactor: modernize gtao chat theme

--- Example_Frameworks/cfx-server-data/resources/[gameplay]/chat-theme-gtao/fxmanifest.lua
+++ Example_Frameworks/cfx-server-data/resources/[gameplay]/chat-theme-gtao/fxmanifest.lua
@@ -1,13 +1,16 @@
--- This resource is part of the default Cfx.re asset pack (cfx-server-data)
--- Altering or recreating for local use only is strongly discouraged.
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
 
-version '1.0.0'
+version '1.1.0'
 author 'Cfx.re <root@cfx.re>'
 description 'A GTA Online-styled theme for the chat resource.'
 repository 'https://github.com/citizenfx/cfx-server-data'
 
-file 'style.css'
-file 'shadow.js'
+files {
+    'style.css',
+    'shadow.js'
+}
 
 chat_theme 'gtao' {
     styleSheet = 'style.css',
@@ -16,6 +19,3 @@ chat_theme 'gtao' {
         default = '<b>{0}</b><span>{1}</span>'
     }
 }
-
-game 'common'
-fx_version 'adamant'
