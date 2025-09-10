841ca29c52073c8a9a4be5e5cf4e33f0ce930bc4
TheRealVSSVSSN <Anthonyadair@hotmail.com>
2025-09-10 09:25:44 -0700

    refactor: modernize money fountain example map

--- Example_Frameworks/cfx-server-data/resources/[gameplay]/[examples]/money-fountain-example-map/map.lua
+++ Example_Frameworks/cfx-server-data/resources/[gameplay]/[examples]/money-fountain-example-map/map.lua
@@ -1,4 +1,15 @@
+--[[
+    -- Type: Money Fountain Map
+    -- Name: test_fountain
+    -- Use: Defines spawn location and payout for sample money fountain
+    -- Created: 2024-05-02
+    -- By: VSSVSSN
+--]]
+
+-- coordinates for the example money fountain
+local fountainCoords = vec3(97.334, -973.621, 29.36)
+
 money_fountain 'test_fountain' {
-    vector3(97.334, -973.621, 29.36),
+    fountainCoords,
     amount = 75
 }
