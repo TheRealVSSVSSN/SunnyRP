40a94426d09aeea60490cd1521c91aefe122d74d
TheRealVSSVSSN <Anthonyadair@hotmail.com>
2025-09-10 15:50:36 -0700

    refactor(np-thermite): modernize resource and UI

--- Example_Frameworks/NoPixelServer/np-thermite/thermite_server.lua
+++ Example_Frameworks/NoPixelServer/np-thermite/thermite_server.lua
@@ -1,11 +1,10 @@
-RegisterServerEvent('thermite:StartFireAtLocation')
-AddEventHandler('thermite:StartFireAtLocation' , function(x,y,z,arg1,arg2)
-    local source = source
-    TriggerClientEvent('thermite:StartClientFires', -1, x,y,z,arg1,arg2)
+RegisterNetEvent('thermite:StartFireAtLocation')
+AddEventHandler('thermite:StartFireAtLocation', function(x, y, z, arg1, arg2)
+    TriggerClientEvent('thermite:StartClientFires', -1, x, y, z, arg1, arg2)
 end)
 
-RegisterServerEvent('thermite:StopFires')
-AddEventHandler('thermite:StopFires' , function()
-    local source = source
+RegisterNetEvent('thermite:StopFires')
+AddEventHandler('thermite:StopFires', function()
     TriggerClientEvent('thermite:StopFiresClient', -1)
 end)
+
