035c189ac83d1f1eb14d24086bc2d5a57cdd9c45
TheRealVSSVSSN <Anthonyadair@hotmail.com>
2025-09-10 16:59:06 -0700

    refactor job tracking and paychecks

--- Example_Frameworks/FiveM-FSN-Framework/fsn_jobs/server.lua
+++ Example_Frameworks/FiveM-FSN-Framework/fsn_jobs/server.lua
@@ -1,7 +1,60 @@
-Citizen.CreateThread(function()
-  while true do
-    Citizen.Wait(600000)
-    print(':fsn_jobs: Paycheck time babies')
-    TriggerClientEvent('fsn_jobs:paycheck', -1)
-  end
+--[[ 
+    -- Type: Module
+    -- Name: fsn_jobs server
+    -- Use: Handles server-side job tracking and paychecks
+    -- Created: 2024-10-??
+    -- By: VSSVSSN
+--]]
+
+local playerJobs = {}
+
+--[[
+    -- Type: Event
+    -- Name: fsn_jobs:updateJob
+    -- Use: Stores a player's current job on the server
+    -- Created: 2024-10-??
+    -- By: VSSVSSN
+--]]
+RegisterNetEvent('fsn_jobs:updateJob', function(job)
+    local src = source
+    playerJobs[src] = job or 'Unemployed'
+end)
+
+--[[
+    -- Type: Event
+    -- Name: playerDropped
+    -- Use: Cleans up job data when a player leaves
+    -- Created: 2024-10-??
+    -- By: VSSVSSN
+--]]
+AddEventHandler('playerDropped', function()
+    playerJobs[source] = nil
+end)
+
+--[[
+    -- Type: Thread
+    -- Name: Paycheck loop
+    -- Use: Sends salary payments based on job every 10 minutes
+    -- Created: 2024-10-??
+    -- By: VSSVSSN
+--]]
+CreateThread(function()
+    while true do
+        Wait(600000)
+        print(':fsn_jobs: distributing paychecks')
+        for src, job in pairs(playerJobs) do
+            local amount = 50
+            if job == 'Police' or job == 'EMS' then
+                amount = 350
+            elseif job == 'Mechanic' or job == 'CarDealer' or job == 'BoatDealer' or job == 'Rancher' then
+                amount = 100
+            elseif job == 'Taxi Driver' then
+                amount = 150
+            elseif job == 'Truck Driver' then
+                amount = 100
+            end
+            TriggerClientEvent('fsn_bank:change:bankAdd', src, amount)
+            TriggerClientEvent('fsn_notify:displayNotification', src, 'Salary: $'..amount, 'centerLeft', 5000, 'info')
+        end
+    end
 end)
