5f13e8db5d87c7a3994810cb0aea70cec857841a
TheRealVSSVSSN <Anthonyadair@hotmail.com>
2025-09-10 15:51:25 -0700

    refactor: update vanilla car tweak resource

---
+++ Example_Frameworks/NoPixelServer/np-vanillaCarTweak/fxmanifest.lua
@@ -0,0 +1,20 @@
+--[[
+    -- Type: Manifest
+    -- Name: np-vanillaCarTweak
+    -- Use: Registers custom handling tweaks for vanilla vehicles
+    -- Created: 2025-09-10
+    -- By: VSSVSSN
+--]]
+
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'VSSVSSN'
+description 'Custom handling tweaks for vanilla vehicles'
+
+files {
+    'handling.meta',
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
