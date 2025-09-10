c74335778abcbde0709a79045688ebd06ceeb4d2
TheRealVSSVSSN <Anthonyadair@hotmail.com>
2025-09-10 09:43:29 -0700

    refactor: modernize rconlog resource

--- Example_Frameworks/cfx-server-data/resources/[system]/rconlog/rconlog_client.lua
+++ Example_Frameworks/cfx-server-data/resources/[system]/rconlog/rconlog_client.lua
@@ -1,6 +1,21 @@
+--[[
+    -- Type: Client Script
+    -- Name: rconlog_client.lua
+    -- Use: Sends player activation and name data to the server for RCON logging
+    -- Created: 2024-05-23
+    -- By: VSSVSSN
+--]]
+
 RegisterNetEvent('rlUpdateNames')
 
-AddEventHandler('rlUpdateNames', function()
+--[[
+    -- Type: Function
+    -- Name: onUpdateNames
+    -- Use: Collects all active player names and forwards them to the server
+    -- Created: 2024-05-23
+    -- By: VSSVSSN
+--]]
+local function onUpdateNames()
     local names = {}
 
     for _, player in ipairs(GetActivePlayers()) do
@@ -8,16 +23,21 @@ AddEventHandler('rlUpdateNames', function()
     end
 
     TriggerServerEvent('rlUpdateNamesResult', names)
-end)
+end
+AddEventHandler('rlUpdateNames', onUpdateNames)
 
+--[[
+    -- Type: Thread
+    -- Name: activationThread
+    -- Use: Waits for the network session and notifies the server once ready
+    -- Created: 2024-05-23
+    -- By: VSSVSSN
+--]]
 CreateThread(function()
-    while true do
-        Wait(0)
-
-        if NetworkIsSessionStarted() then
-            TriggerServerEvent('rlPlayerActivated')
-
-            return
-        end
+    while not NetworkIsSessionStarted() do
+        Wait(500)
     end
+
+    TriggerServerEvent('rlPlayerActivated')
 end)
+
