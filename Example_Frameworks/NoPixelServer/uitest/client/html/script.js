a0b932b455a37bc87f541c22190a18070833c497
TheRealVSSVSSN <Anthonyadair@hotmail.com>
2025-09-10 16:45:27 -0700

    refactor uitest to fxmanifest and vanilla nui

--- Example_Frameworks/NoPixelServer/uitest/client/html/script.js
+++ Example_Frameworks/NoPixelServer/uitest/client/html/script.js
@@ -1,301 +1,186 @@
-var documentWidth = document.documentElement.clientWidth;
-var documentHeight = document.documentElement.clientHeight;
+const cursor = document.getElementById('cursor');
+let cursorX = window.innerWidth / 2;
+let cursorY = window.innerHeight / 2;
 
-var cursor = document.getElementById("cursor");
-var cursorX = documentWidth / 2;
-var cursorY = documentHeight / 2;
+let btnCount = 0;
+let btnSelected = 0;
+let currentMenu = 1;
+let buttonHeight = 0;
 
-var btnCount = 0;
-var btnSelected = 0;
-
-var currentAmountOfMenus = 1
-var buttonHeight = 0
-
-function UpdateCursorPos() {
-    cursor.style.left = cursorX;
-    cursor.style.top = cursorY;
+function updateCursor() {
+  cursor.style.left = `${cursorX}px`;
+  cursor.style.top = `${cursorY}px`;
 }
 
-function Click(x, y) {
-    var element = $(document.elementFromPoint(x, y));
-    element.focus().click();
+function clickAt(x, y) {
+  const el = document.elementFromPoint(x, y);
+  if (el) {
+    el.focus();
+    el.click();
+  }
 }
 
-$(function() {
-    window.addEventListener('message', function(event) {
-        if (event.data.type == "enableui") {
-            $( "#login-form" ).empty();
-            btnCount = 0;
-            btnSelected = 0;
-            cursor.style.display = event.data.enable ? "block" : "none";
-            document.body.style.display = event.data.enable ? "block" : "none";
-        } else if (event.data.type == "endOfCurrentMenu") {
-            if (event.data.isEnd == true)
-            {
-                if (currentAmountOfMenus > 1)
-                {
-                    $('.menu'+currentAmountOfMenus).css('left', (-320)+(currentAmountOfMenus*280));
-                }
-                currentAmountOfMenus++
-                buttonHeight = 0
-            }
-            else if (event.data.isEnd == false)
-            {
-                currentAmountOfMenus = 0
-            }
-        } else if (event.data.type == "addButton") {
-            
-            var f = document.getElementById("login-form");
-            var xx = document.getElementById("dialog");
-            xx.setAttribute('style',"fixed; top: 50%; left: 50%; transform: translate(-50%, -50%)");
-            btnCount++;
-            var i = document.createElement("button");
-            event.id = btnCount;
-            var startingHeight = -170
-            var startingLeft = 10
-            if(event.data.buttonType == "animSets")
-            {
-                startingHeight = -470
-            }
+function post(action, data = {}) {
+  fetch(`https://uitest/${action}`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json; charset=UTF-8' },
+    body: JSON.stringify(data)
+  });
+}
 
-            if(event.data.buttonType == "tattoo")
-            {
-                startingLeft = 810
-            }
-
-
-            if(event.data.buttonType == "hair2")
-            {
-                startingLeft = 810
-                startingHeight = -1147
-            }
-
-            if(event.data.buttonType == "hair")
-            {
-                startingLeft = 550
-                startingHeight = -560
-            }
-
-
-            if(event.data.buttonType == "hairclose")
-            {
-                startingLeft = -260
-                startingHeight = 420
-
-            }
-            if(event.data.buttonType == "haircloseaids")
-            {
-                startingLeft = 0
-                startingHeight = 330                
-            }
-
-            if(event.data.buttonType == "hairnextprev")
-            {
-                startingLeft = 260
-                startingHeight = 285
-            }
-
-
-            i.setAttribute('id', 'btn' + btnCount);
-            i.setAttribute('class',"button button1 menu"+currentAmountOfMenus);
-            i.setAttribute('value',event.data.functionname);
-            i.setAttribute('type',"button");
-            i.setAttribute('menu',currentAmountOfMenus);
-            i.style.left = (startingLeft+(currentAmountOfMenus*280))
-            i.style.top = (startingHeight+(buttonHeight*45))
-            i.innerHTML = event.data.name;
-
-            if(event.data.buttonType == "police")
-            {
-                i.style.backgroundImage = "url('nui://uitest/client/html/menuButtonPolice.png')"
-            }
-            else if (event.data.buttonType == "medic")
-            {
-                i.style.backgroundImage = "url('nui://uitest/client/html/menuButtonEms.png')"
-            }
-            else if (event.data.buttonType == "judge")
-            {
-                i.style.backgroundImage = "url('nui://uitest/client/html/menuButtonJudge.png')"
-            }
-            else
-            {
-                i.style.backgroundImage = "url('nui://uitest/client/html/menuButton.png')"
-            }
-
-            buttonHeight++
-            if (currentAmountOfMenus > 1)
-            {
-                for (var j = currentAmountOfMenus; j >= 0; j--) {
-                    if (j != currentAmountOfMenus)
-                    {
-                        $('.menu'+j).css('left', (-320)+(j*280));
-                    }   
-                }
-            }
-            i.onclick = function () { 
-                var menu = $( "#btn" + btnSelected ).attr("menu")
-                if (menu == currentAmountOfMenus-1)
-                {
-                    $.post('http://uitest/runfunction', JSON.stringify({  functionset: event.data.functionname , name: event.data.name, buttonType: event.data.buttonType })); 
-                }
-                else
-                {
-                    if (event.data.functionname == "openSubMenu")
-                    {
-                        if (menu == 0)
-                        {   
-                            for (var m = currentAmountOfMenus-1; m >= 1; m--) {
-                                currentAmountOfMenus--
-                                $('.menu'+0).css('left',(10+(0*280)));
-                                var element = document.getElementsByClassName("menu"+m), index;
-                                if(element !== null)
-                                {
-                                    for (index = element.length - 1; index >= 0; index--) {
-                                      element[index].parentNode.removeChild(element[index]);
-                                    }
-                                }
-                            }
-                        }
-                        else
-                        {
-                            var menuStop = currentAmountOfMenus - menu
-                            for (var m = currentAmountOfMenus ; m >= menuStop; m--) {
-                                currentAmountOfMenus = menuStop
-                                var element = document.getElementsByClassName("menu"+m), index;
-                                if(element !== null)
-                                {
-                                    for (index = element.length - 1; index >= 0; index--) {
-                                      element[index].parentNode.removeChild(element[index]);
-                                    }
-                                }
-                            }
-                        }
-                         $.post('http://uitest/runfunction', JSON.stringify({  functionset: event.data.functionname , name: event.data.name, buttonType: event.data.buttonType })); 
-                    }
-                    else
-                    {
-                        $.post('http://uitest/runfunction', JSON.stringify({  functionset: event.data.functionname , name: event.data.name, buttonType: event.data.buttonType })); 
-                    }
-                }
-                
-            }; 
-            i.onmouseover = function () {
-                $( "#btn" + btnSelected ).removeClass('selected');
-                btnSelected = event.id;
-               
-            };
-            f.appendChild(i);
-
-            if (btnCount == 1) {
-                btnSelected = btnCount;
-                $( "#btn" + btnSelected ).addClass('selected');
-            }
-
-        } else if (event.data.type == "addButton3") {
-            var f = document.getElementById("login-form");
-
-            var xx = document.getElementById("dialog");
-            xx.setAttribute('style',"fixed; top: 50%; margin-left: 35%; width:35%; transform: translate(-50%, -50%)");
-
-            btnCount++;
-            var i = document.createElement("button");
-            event.id = btnCount;
-            i.setAttribute('id', 'btn' + btnCount);
-            i.setAttribute('class',"button button2");
-            i.setAttribute('value',event.data.functionname);
-            i.setAttribute('type',"button");
-            i.innerHTML = event.data.name;
-            i.onclick = function () { $.post('http://uitest/runfunction', JSON.stringify({  functionset: event.data.functionname })); }; 
-            i.onmouseover = function () {
-                $( "#btn" + btnSelected ).removeClass('selected');
-                btnSelected = event.id;
-                i.addClass('selected');
-            };
-            f.appendChild(i);
-
-            if (btnCount == 1) {
-                btnSelected = btnCount;
-                $( "#btn" + btnSelected ).addClass('selected');
-            }
-        } else if (event.data.type == "click") {
-            // Avoid clicking the cursor itself, click 1px to the top/left;
-            btnDeSelect();
-            Click(cursorX - 1, cursorY - 1);
-        } else if (event.data.type == "up") {
-            btnUp();
-        } else if (event.data.type == "down") {
-            btnDown();
+window.addEventListener('message', (event) => {
+  const data = event.data;
+  if (data.type === 'enableui') {
+    const form = document.getElementById('login-form');
+    form.innerHTML = '';
+    btnCount = 0;
+    btnSelected = 0;
+    cursor.style.display = data.enable ? 'block' : 'none';
+    document.body.style.display = data.enable ? 'block' : 'none';
+  } else if (data.type === 'endOfCurrentMenu') {
+    if (data.isEnd) {
+      if (currentMenu > 1) {
+        const prevMenu = document.querySelector(`.menu${currentMenu}`);
+        if (prevMenu) {
+          prevMenu.style.left = `${-320 + currentMenu * 280}px`;
         }
+      }
+      currentMenu += 1;
+      buttonHeight = 0;
+    } else {
+      currentMenu = 0;
+    }
+  } else if (data.type === 'addButton') {
+    const form = document.getElementById('login-form');
+    const btn = document.createElement('button');
+    btnCount += 1;
+    const id = `btn${btnCount}`;
+    btn.id = id;
+    btn.className = `button button1 menu${currentMenu}`;
+    btn.value = data.functionname;
+    btn.type = 'button';
+    btn.dataset.menu = currentMenu;
 
-    });
+    let startTop = -170;
+    let startLeft = 10;
 
-    $(document).mousemove(function(event) {
-        cursorX = event.pageX;
-        cursorY = event.pageY;
-        UpdateCursorPos();
-        btnDeSelect();
-    });
+    if (data.buttonType === 'animSets') startTop = -470;
+    if (data.buttonType === 'tattoo') startLeft = 810;
+    if (data.buttonType === 'hair2') { startLeft = 810; startTop = -1147; }
+    if (data.buttonType === 'hair') { startLeft = 550; startTop = -560; }
+    if (data.buttonType === 'hairclose') { startLeft = -260; startTop = 420; }
+    if (data.buttonType === 'haircloseaids') { startLeft = 0; startTop = 330; }
+    if (data.buttonType === 'hairnextprev') { startLeft = 260; startTop = 285; }
 
-    document.onkeyup = function (data) {
-        if (data.which == 27) { // Escape key
-           $.post('http://uitest/escape', JSON.stringify({}));
-        }
-        if (data.which == 113) { // f2
-           $.post('http://uitest/escape', JSON.stringify({}));
-        }
-        if (data.which == 37) { // left
-            $.post('http://uitest/left', JSON.stringify({}));
-        }
-        if (data.which == 39) { // right
-            $.post('http://uitest/right', JSON.stringify({}));
-        }
-        if (data.which == 38) { // Up
-            $.post('http://uitest/up', JSON.stringify({}));
-        }
-        if (data.which == 40) { // Down
-            $.post('http://uitest/down', JSON.stringify({}));
-        }
-        if (data.which == 13) { // Enter
-            btnEnter();
-        }
+    btn.style.left = `${startLeft + currentMenu * 280}px`;
+    btn.style.top = `${startTop + buttonHeight * 45}px`;
+    btn.textContent = data.name;
+
+    if (data.buttonType === 'police') {
+      btn.style.backgroundImage = "url('nui://uitest/client/html/menuButtonPolice.png')";
+    } else if (data.buttonType === 'medic') {
+      btn.style.backgroundImage = "url('nui://uitest/client/html/menuButtonEms.png')";
+    } else if (data.buttonType === 'judge') {
+      btn.style.backgroundImage = "url('nui://uitest/client/html/menuButtonJudge.png')";
+    } else {
+      btn.style.backgroundImage = "url('nui://uitest/client/html/menuButton.png')";
+    }
+
+    btn.onclick = () => {
+      post('runfunction', { functionset: data.functionname, buttonType: data.buttonType, name: data.name });
     };
 
-    $("#login-form button").click(function (ev) {
-        ev.preventDefault(); // cancel form submission
+    btn.onmouseover = () => {
+      const prev = document.getElementById(`btn${btnSelected}`);
+      if (prev) prev.classList.remove('selected');
+      btnSelected = btnCount;
+      btn.classList.add('selected');
+    };
 
-        if ($(this).attr("value") == "close") {
-            //do button 1 thing
-             $.post('http://uitest/escape', JSON.stringify({}));
-        }
+    form.appendChild(btn);
+    buttonHeight += 1;
 
-    });
-
-    function btnUp() {
-        $( "#btn" + btnSelected ).removeClass('selected');
-        btnSelected--;
-        if (btnSelected <= 0) {
-            btnSelected = btnCount;
-        }
-        $( "#btn" + btnSelected ).addClass('selected');
+    if (btnCount === 1) {
+      btnSelected = 1;
+      btn.classList.add('selected');
     }
-
-    function btnDown() {
-        $( "#btn" + btnSelected ).removeClass('selected');
-        btnSelected++;
-        if (btnSelected > btnCount) {
-            btnSelected = 1;
-        }
-        $( "#btn" + btnSelected ).addClass('selected');
+  } else if (data.type === 'addButton3') {
+    const form = document.getElementById('login-form');
+    const btn = document.createElement('button');
+    btnCount += 1;
+    const id = `btn${btnCount}`;
+    btn.id = id;
+    btn.className = 'button button2';
+    btn.value = data.functionname;
+    btn.type = 'button';
+    btn.textContent = data.name;
+    btn.onclick = () => post('runfunction', { functionset: data.functionname });
+    btn.onmouseover = () => {
+      const prev = document.getElementById(`btn${btnSelected}`);
+      if (prev) prev.classList.remove('selected');
+      btnSelected = btnCount;
+      btn.classList.add('selected');
+    };
+    form.appendChild(btn);
+    if (btnCount === 1) {
+      btnSelected = 1;
+      btn.classList.add('selected');
     }
-
-    function btnEnter() {
-        if (btnSelected > 0) {
-            $( "#btn" + btnSelected )[0].onclick();
-        }
-    }
-
-    function btnDeSelect() {        
-        $( "#btn" + btnSelected ).removeClass('selected');
-    }
-
+  } else if (data.type === 'click') {
+    const prev = document.getElementById(`btn${btnSelected}`);
+    if (prev) prev.classList.remove('selected');
+    clickAt(cursorX - 1, cursorY - 1);
+  } else if (data.type === 'up') {
+    btnUp();
+  } else if (data.type === 'down') {
+    btnDown();
+  }
 });
+
+document.addEventListener('mousemove', (event) => {
+  cursorX = event.pageX;
+  cursorY = event.pageY;
+  updateCursor();
+  btnDeSelect();
+});
+
+document.addEventListener('keyup', (e) => {
+  if (e.which === 27 || e.which === 113) post('escape');
+  if (e.which === 37) post('left');
+  if (e.which === 39) post('right');
+  if (e.which === 38) post('up');
+  if (e.which === 40) post('down');
+  if (e.which === 13) btnEnter();
+});
+
+function btnUp() {
+  const prev = document.getElementById(`btn${btnSelected}`);
+  if (prev) prev.classList.remove('selected');
+  btnSelected -= 1;
+  if (btnSelected <= 0) btnSelected = btnCount;
+  const btn = document.getElementById(`btn${btnSelected}`);
+  if (btn) btn.classList.add('selected');
+}
+
+function btnDown() {
+  const prev = document.getElementById(`btn${btnSelected}`);
+  if (prev) prev.classList.remove('selected');
+  btnSelected += 1;
+  if (btnSelected > btnCount) btnSelected = 1;
+  const btn = document.getElementById(`btn${btnSelected}`);
+  if (btn) btn.classList.add('selected');
+}
+
+function btnEnter() {
+  if (btnSelected > 0) {
+    const btn = document.getElementById(`btn${btnSelected}`);
+    if (btn && typeof btn.onclick === 'function') {
+      btn.onclick();
+    }
+  }
+}
+
+function btnDeSelect() {
+  const btn = document.getElementById(`btn${btnSelected}`);
+  if (btn) btn.classList.remove('selected');
+}
--- Example_Frameworks/NoPixelServer/uitest/fxmanifest.lua
+++ Example_Frameworks/NoPixelServer/uitest/fxmanifest.lua
@@ -1,10 +1,14 @@
-resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
-client_script "@np-errorlog/client/cl_errorlog.lua"
-client_script('client/client.lua')
+fx_version 'cerulean'
+game 'gta5'
 
-ui_page('client/html/index.html')
+lua54 'yes'
 
-files({
+client_script '@np-errorlog/client/cl_errorlog.lua'
+client_script 'client/client.lua'
+
+ui_page 'client/html/index.html'
+
+files {
     'client/html/index.html',
     'client/html/script.js',
     'client/html/style.css',
@@ -13,4 +17,4 @@ files({
     'client/html/menuButtonJudge.png',
     'client/html/menuButtonEms.png',
     'client/html/menuButtonPolice.png'
-})
+}
