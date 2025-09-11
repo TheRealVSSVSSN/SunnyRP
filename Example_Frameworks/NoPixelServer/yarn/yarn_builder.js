5babb2860869ba4b3a7a0a6d63f99b78fb3789c7
TheRealVSSVSSN <Anthonyadair@hotmail.com>
2025-09-10 17:05:33 -0700

    refactor: modernize yarn build task

--- Example_Frameworks/NoPixelServer/yarn/yarn_builder.js
+++ Example_Frameworks/NoPixelServer/yarn/yarn_builder.js
@@ -1,80 +1,75 @@
 const path = require('path');
-const fs = require('fs');
-const child_process = require('child_process');
-let buildingInProgress = false;
-let currentBuildingModule = '';
+const { promises: fs } = require('fs');
+const { fork } = require('child_process');
 
-const yarnBuildTask = {
-	shouldBuild(resourceName) {
-		try {
-			const resourcePath = GetResourcePath(resourceName);
-			
-			const packageJson = path.resolve(resourcePath, 'package.json');
-			const yarnLock = path.resolve(resourcePath, 'yarn.lock');
-			
-			const packageStat = fs.statSync(packageJson);
-			
-			try {
-				const yarnStat = fs.statSync(yarnLock);
-				
-				if (packageStat.mtimeMs > yarnStat.mtimeMs) {
-					return true;
-				}
-			} catch (e) {
-				// no yarn.lock, but package.json - install time!
-				return true;
-			}
-		} catch (e) {
-			
-		}
-		
-		return false;
-	},
-	
-	build(resourceName, cb) {
-		let buildYarn = async () => {
-			while (buildingInProgress) {
-				console.log(`yarn is busy by another process: we are waiting to compile  ${resourceName}`);
-				await sleep(3000);
-			}
-			buildingInProgress = true;
-			currentBuildingModule = resourceName;
-			const process = child_process.fork(
-				require.resolve('./yarn_cli.js'),
-				['install', '--ignore-scripts'],
-				{
-					cwd: path.resolve(GetResourcePath(resourceName))
-				});
+class YarnBuildTask {
+  constructor() {
+    this.building = false;
+  }
 
-			process.on('exit', (code, signal) => {
-				setImmediate(() => {
-					if (code != 0 || signal) {
-						buildingInProgress = false;
-						currentBuildingModule = '';
-						cb(false, 'yarn failed!');
-						return;
-					}
+  async shouldBuild(resourceName) {
+    try {
+      const resourcePath = GetResourcePath(resourceName);
+      const packageJson = path.join(resourcePath, 'package.json');
+      const yarnLock = path.join(resourcePath, 'yarn.lock');
 
-					const resourcePath = GetResourcePath(resourceName);
-					const yarnLock = path.resolve(resourcePath, 'yarn.lock');
+      const pkgStat = await fs.stat(packageJson);
+      try {
+        const lockStat = await fs.stat(yarnLock);
+        return pkgStat.mtimeMs > lockStat.mtimeMs;
+      } catch {
+        // yarn.lock missing - need to install
+        return true;
+      }
+    } catch (err) {
+      console.error(`yarn build check failed for ${resourceName}:`, err);
+      return false;
+    }
+  }
 
-					try {
-						fs.utimesSync(yarnLock, new Date(), new Date());
-					} catch (e) {
+  async build(resourceName, cb) {
+    try {
+      while (this.building) {
+        console.log(`yarn is busy by another process: we are waiting to compile ${resourceName}`);
+        await delay(3000);
+      }
+      this.building = true;
 
-					}
+      await new Promise((resolve, reject) => {
+        const child = fork(require.resolve('./yarn_cli.js'), ['install', '--ignore-scripts'], {
+          cwd: path.resolve(GetResourcePath(resourceName)),
+        });
 
-					buildingInProgress = false;
-					currentBuildingModule = '';
-					cb(true);
-				});
-			});
-		};
-		buildYarn().then();
-	}
-};
+        child.on('exit', (code, signal) => {
+          if (code !== 0 || signal) {
+            reject(new Error('yarn failed'));
+          } else {
+            resolve();
+          }
+        });
+      });
 
-function sleep(ms) {
-	return new Promise(resolve => setTimeout(resolve, ms));
+      const yarnLock = path.join(GetResourcePath(resourceName), 'yarn.lock');
+      const now = new Date();
+      try {
+        await fs.utimes(yarnLock, now, now);
+      } catch (err) {
+        console.warn(`failed to update yarn.lock mtime for ${resourceName}:`, err);
+      }
+
+      cb(true);
+    } catch (err) {
+      console.error(err);
+      cb(false, err.message || 'yarn failed');
+    } finally {
+      this.building = false;
+    }
+  }
 }
-RegisterResourceBuildTaskFactory('yarn', () => yarnBuildTask);
+
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+RegisterResourceBuildTaskFactory('yarn', () => new YarnBuildTask());
+
