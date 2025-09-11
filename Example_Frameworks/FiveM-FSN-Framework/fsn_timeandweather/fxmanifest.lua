62fc42aa2334a7d04b155ecdefaf85a5baa408b2
TheRealVSSVSSN <Anthonyadair@hotmail.com>
2025-09-10 17:12:43 -0700

    refactor: modernize FSN time and weather

--- Example_Frameworks/FiveM-FSN-Framework/fsn_timeandweather/fxmanifest.lua
+++ Example_Frameworks/FiveM-FSN-Framework/fsn_timeandweather/fxmanifest.lua
@@ -1,16 +1,37 @@
---[[/	:FSN:	\]]--
-fx_version 'adamant'
+--[[/   :FSN:   \]]--
+fx_version 'cerulean'
 game 'gta5'
+lua54 'yes'
 
-client_script '@fsn_main/cl_utils.lua'
-server_script '@fsn_main/sv_utils.lua'
-client_script '@fsn_main/server_settings/sh_settings.lua'
-server_script '@fsn_main/server_settings/sh_settings.lua'
-server_script '@mysql-async/lib/MySQL.lua'
---[[/	:FSN:	\]]--
+client_scripts {
+    '@fsn_main/cl_utils.lua',
+    '@fsn_main/server_settings/sh_settings.lua',
+    'client.lua'
+}
 
-client_script 'client.lua'
-server_script 'server.lua'
+server_scripts {
+    '@fsn_main/sv_utils.lua',
+    '@fsn_main/server_settings/sh_settings.lua',
+    'server.lua'
+}
+
+files {
+    'timecycle_mods_4.xml',
+    'w_blizzard.xml',
+    'w_clear.xml',
+    'w_clearing.xml',
+    'w_clouds.xml',
+    'w_extrasunny.xml',
+    'w_foggy.xml',
+    'w_neutral.xml',
+    'w_overcast.xml',
+    'w_rain.xml',
+    'w_smog.xml',
+    'w_snow.xml',
+    'w_snowlight.xml',
+    'w_thunder.xml',
+    'w_xmas.xml'
+}
 
 data_file 'TIMECYCLEMOD_FILE' 'timecycle_mods_4.xml'
 data_file 'TIMECYCLEMOD_FILE' 'w_blizzard.xml'
@@ -28,24 +49,9 @@ data_file 'TIMECYCLEMOD_FILE' 'w_snowlight.xml'
 data_file 'TIMECYCLEMOD_FILE' 'w_thunder.xml'
 data_file 'TIMECYCLEMOD_FILE' 'w_xmas.xml'
 
-
-files {
-	'timecycle_mods_4.xml',
-	'w_blizzard.xml',
-	'w_clear.xml',
-	'w_clearing.xml',
-	'w_clouds.xml',
-	'w_extrasunny.xml',
-	'w_foggy.xml',
-	'w_neutral.xml',
-	'w_overcast.xml',
-	'w_rain.xml',
-	'w_smog.xml',
-	'w_snow.xml',
-	'w_snowlight.xml',
-	'w_thunder.xml',
-	'w_xmas.xml'
+exports {
+    'getTime'
 }
-exports({
-  'getTime',
-})
+
+--[[/   :FSN:   \]]--
+
