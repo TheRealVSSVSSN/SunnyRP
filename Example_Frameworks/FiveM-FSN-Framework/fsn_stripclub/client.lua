b9f2be8c9605ab5ad0458c7f1bc612e1b3bbf819
TheRealVSSVSSN <Anthonyadair@hotmail.com>
2025-09-10 17:11:53 -0700

    refactor(stripclub): modernize booth management

--- Example_Frameworks/FiveM-FSN-Framework/fsn_stripclub/client.lua
+++ Example_Frameworks/FiveM-FSN-Framework/fsn_stripclub/client.lua
@@ -10,19 +10,30 @@ local positions = {
     inuse = false
   }
 }
-local hookers = {"Stripper01SFY", "Stripper02SFY", "StripperLiteSFY", "Hooker01SFY", "Hooker02SFY", "Hooker03SFY"}
+local hookers = {"s_f_y_stripper_01", "s_f_y_stripper_02", "s_f_y_stripperlite", "s_f_y_hooker_01", "s_f_y_hooker_02", "s_f_y_hooker_03"}
 local curbooth = -1
-local stripper_cam = false
-local stripper = false
+local stripper = nil
 
 RegisterNetEvent('fsn_stripclub:client:update')
 AddEventHandler('fsn_stripclub:client:update', function(tbl)
   positions = tbl
 end)
 
-function drawTxt(text,font,centre,x,y,scale,r,g,b,a)
+-- Request current booth state from the server on script load
+CreateThread(function()
+  TriggerServerEvent('fsn_stripclub:server:sync')
+end)
+
+--[[
+    -- Type: Function
+    -- Name: drawTxt
+    -- Use: Render on-screen instructional text
+    -- Created: 2024-04-20
+    -- By: VSSVSSN
+--]]
+local function drawTxt(text,font,centre,x,y,scale,r,g,b,a)
   SetTextFont(font)
-  SetTextProportional(0)
+  SetTextProportional(false)
   SetTextScale(scale, scale)
   SetTextColour(r, g, b, a)
   SetTextDropShadow(0, 0, 0, 0,255)
@@ -35,58 +46,95 @@ function drawTxt(text,font,centre,x,y,scale,r,g,b,a)
   DrawText(x , y)
 end
 
-function StartLapDance(id)
+--[[
+    -- Type: Function
+    -- Name: StartLapDance
+    -- Use: Initiates lap dance sequence for given booth
+    -- Created: 2024-04-20
+    -- By: VSSVSSN
+--]]
+local function StartLapDance(id)
   if positions[id].inuse then
     TriggerEvent('fsn_notify:displayNotification', 'This booth is already in use numnuts', 'centerLeft', 3000, 'error')
-  else
-    curbooth = id
-    TriggerServerEvent('fsn_stripclub:server:claimBooth', id)
-    local pos = positions[id]
-    SetEntityHeading(PlayerPedId(), pos.sitpos.h)
-    SetEntityCoords(PlayerPedId(), pos.sitpos.x, pos.sitpos.y, pos.sitpos.z, true, true, true, false)
-    TriggerEvent('fsn_emotecontrol:play', 'anim', 'switch@michael@sitting', 'idle')
-
-    local mdl = -1360365899
-    RequestModel(mdl)
-    while not HasModelLoaded(mdl) do
-        Citizen.Wait(1)
-        RequestModel(mdl)
-        print('loading model')
-    end
-    stripper = CreatePed(1, mdl, pos.dancerpos.front.x, pos.dancerpos.front.y, pos.dancerpos.front.z, pos.dancerpos.front.h, false, true)
-
-    --stripper_cam = CreateCam("DEFAULT_SCRIPTED_CAMERA", 1)
-    --SetCamCoord(stripper_cam, pos.campos.x, pos.campos.y, pos.campos.z)
-    --local currot = GetCamRot(stripper_cam, 2)
-    --AttachCamToEntity(stripper_cam, stripper, 0.0,0.0,-1.5, true)
-    --RenderScriptCams(1, 0, 0, 1, 1)
-
-    Citizen.Wait(500)
-    DoScreenFadeIn(1000)
-    FreezeEntityPosition(PlayerPedId(), true)
+    return
   end
+
+  curbooth = id
+  TriggerServerEvent('fsn_stripclub:server:claimBooth', id)
+
+  local pos = positions[id]
+  local ped = PlayerPedId()
+  SetEntityHeading(ped, pos.sitpos.h)
+  SetEntityCoords(ped, pos.sitpos.x, pos.sitpos.y, pos.sitpos.z, false, false, false, false)
+  TriggerEvent('fsn_emotecontrol:play', 'anim', 'switch@michael@sitting', 'idle')
+
+  local modelName = hookers[math.random(#hookers)]
+  local model = GetHashKey(modelName)
+  RequestModel(model)
+  while not HasModelLoaded(model) do
+    Wait(0)
+  end
+  stripper = CreatePed(1, model, pos.dancerpos.front.x, pos.dancerpos.front.y, pos.dancerpos.front.z, pos.dancerpos.front.h, false, true)
+  SetModelAsNoLongerNeeded(model)
+  SetBlockingOfNonTemporaryEvents(stripper, true)
+
+  Citizen.CreateThread(function()
+    local animDict = "mini@strip_club@lap_dance@ld_girl_a_song_a_p1"
+    RequestAnimDict(animDict)
+    while not HasAnimDictLoaded(animDict) do
+      Wait(0)
+    end
+    TaskPlayAnim(stripper, animDict, "ld_girl_a_song_a_p1_f", 8.0, -8.0, -1, 1, 0.0, false, false, false)
+  end)
+
+  Wait(500)
+  DoScreenFadeIn(1000)
+  FreezeEntityPosition(ped, true)
+end
+
+--[[
+    -- Type: Function
+    -- Name: StopLapDance
+    -- Use: Ends lap dance and releases booth
+    -- Created: 2024-04-20
+    -- By: VSSVSSN
+--]]
+local function StopLapDance()
+  if curbooth < 0 then return end
+
+  local ped = PlayerPedId()
+  FreezeEntityPosition(ped, false)
+  TriggerServerEvent('fsn_stripclub:server:releaseBooth', curbooth)
+
+  if stripper and DoesEntityExist(stripper) then
+    DeleteEntity(stripper)
+    stripper = nil
+  end
+
+  curbooth = -1
 end
 DoScreenFadeIn(1000)
-Citizen.CreateThread(function()
+CreateThread(function()
   local bleep = AddBlipForCoord(115.66896057129, -1296.7384033203, 29.269014358521)
   SetBlipSprite(bleep, 121)
   SetBlipScale(bleep, 0.8)
   SetBlipAsShortRange(bleep, true)
   BeginTextCommandSetBlipName("STRING")
-    AddTextComponentString("Strip Club")
+  AddTextComponentString("Strip Club")
   EndTextCommandSetBlipName(bleep)
-  while true do
-    Citizen.Wait(0)
-    if curbooth > 0 then
-      local boof = positions[curbooth]
-      local currot = GetCamRot(stripper_cam, 2)
-      drawTxt('CamRot:\nX: '..currot.x..'\nY: '..currot.y..'\nZ: '..currot.z,4,0,20,20,0.6,255,255,255,255)
 
+  while true do
+    Wait(0)
+    if curbooth > 0 then
+      drawTxt("Press ~INPUT_FRONTEND_CANCEL~ to stop", 4, 0, 0.45, 0.9, 0.5, 255, 255, 255, 200)
+      if IsControlJustPressed(0, 202) then -- backspace
+        StopLapDance()
+      end
     else
       for k, v in pairs(positions) do
-        if GetDistanceBetweenCoords(v.pos.x, v.pos.y, v.pos.z, GetEntityCoords(PlayerPedId()), true) < 5 then
-          DrawMarker(1,v.pos.x, v.pos.y, v.pos.z-1,0,0,0,0,0,0,1.0, 1.0, 0.5,0,155,255,175,0,0,0,0)
-          if GetDistanceBetweenCoords(v.pos.x, v.pos.y, v.pos.z, GetEntityCoords(PlayerPedId()), true) < 1 then
+        if #(GetEntityCoords(PlayerPedId()) - vector3(v.pos.x, v.pos.y, v.pos.z)) < 5.0 then
+          DrawMarker(1, v.pos.x, v.pos.y, v.pos.z - 1, 0, 0, 0, 0, 0, 0, 1.0, 1.0, 0.5, 0, 155, 255, 175, false, true, 2, false, nil, nil, false)
+          if #(GetEntityCoords(PlayerPedId()) - vector3(v.pos.x, v.pos.y, v.pos.z)) < 1.0 then
             if v.inuse then
               SetTextComponentFormat("STRING")
               AddTextComponentString("~r~This booth is already in use")
@@ -105,4 +153,10 @@ Citizen.CreateThread(function()
       end
     end
   end
+end)
+
+-- Ensure booth is freed if the resource stops
+AddEventHandler('onClientResourceStop', function(res)
+  if res ~= GetCurrentResourceName() then return end
+  StopLapDance()
 end)
